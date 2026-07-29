89a972ebf8cc3190fb365f9bdb479719f13105c9
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-07-29 20:55:20 +0300

    feat: add base Component class in component.ts (Wed, Jul 29, 2026 08:55:20 PM)

---
+++ src/core/component.ts
@@ -0,0 +1,57 @@
+type ComponentProps = Record<string, any>;
+type ComponentState = Record<string, any>;
+
+interface ComponentOptions {
+  tagName?: string;
+  className?: string;
+  props?: ComponentProps;
+}
+
+export class Component {
+  props: ComponentProps;
+  state: ComponentState;
+  element: HTMLElement;
+
+  constructor(options: ComponentOptions = {}) {
+    const { tagName = 'div', className = '', props = {} } = options;
+
+    this.props = props;
+    this.state = {};
+
+    this.element = document.createElement(tagName);
+    if (className) this.element.className = className;
+  }
+
+  setState(newState: ComponentState): void {
+    const prevState: ComponentState = { ...this.state };
+    this.state = { ...prevState, ...newState };
+    this.onStateChanges(prevState, this.state);
+    this.update();
+  }
+
+  onStateChanges(prevState: ComponentState, newState: ComponentState): void {}
+  onMount(): void {}
+  onUnmount(): void {}
+
+  render(): string {
+    return '';
+  }
+
+  update(): void {
+    this.element.innerHTML = this.render();
+    this.afterRender();
+  }
+
+  afterRender() {}
+
+  mount(container: HTMLElement): void {
+    this.update();
+    container.appendChild(this.element);
+    this.onMount();
+  }
+
+  unmount(): void {
+    this.onUnmount();
+    this.element.remove();
+  }
+}
