1b6c97e8f7630e62fc65e4dc468bae9487af09bf
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-08-01 21:26:05 +0300

    feat: add PodcastCard component (Sat, Aug 01, 2026 09:26:04 PM)

---
+++ src/components/podcast-card.ts
@@ -0,0 +1,32 @@
+import { Component } from '../core/component.ts';
+import type { Podcast } from '../types/types.ts';
+
+export class PodcastCard extends Component {
+  constructor(props: Podcast) {
+    super({
+      tagName: 'article',
+      className: 'card',
+      dataset: { id: props.id },
+      props,
+    });
+  }
+
+  render(): string {
+    const podcast = this.props;
+
+    return `
+      <div class="card-cover-wrapper">
+        <img 
+          class="card-img" 
+          src="${podcast.coverUrl}" 
+          alt="${podcast.title}" 
+          loading="lazy" 
+        />
+      </div>
+      <div class="card-content">
+        <h3 class="card-title">${podcast.title}</h3>
+        <p class="card-author">${podcast.author}</p>
+      </div>
+    `;
+  }
+}
