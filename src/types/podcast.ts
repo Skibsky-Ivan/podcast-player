18e96d691b6106ff46bae4a0a86b4965139b1401
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-08-01 17:20:34 +0300

    feat: add podcast and episode typescript interfaces (Sat, Aug 01, 2026 05:20:34 PM)

---
+++ src/types/podcast.ts
@@ -0,0 +1,14 @@
+export interface Podcast {
+  id: string;
+  title: string;
+  author: string;
+  coverUrl: string;
+  numberEpisode: string;
+}
+
+export interface Episode {
+  id: string;
+  title: string;
+  author: string;
+  duration: string;
+}
