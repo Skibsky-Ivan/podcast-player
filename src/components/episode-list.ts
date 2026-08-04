3693c304e74f4991fafcd1acbbf49ea6db400c93
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-08-04 17:39:09 +0300

    feat(components): add EpisodeList layout component (Tue, Aug 04, 2026 05:39:09 PM)

---
+++ src/components/episode-list.ts
@@ -0,0 +1,26 @@
+import { Component } from '../core/component';
+
+export class EpisodeList extends Component {
+  constructor() {
+    super({
+      tagName: 'section',
+      className: 'tracklist-container',
+    });
+  }
+
+  render(): string {
+    return `
+      <div class="tracklist-header">
+        <span class="header-num">#</span>
+        <span class="header-title">Название</span>
+        <span class="header-time">
+          <span class="icon icon-clock"></span>
+        </span>
+      </div>
+
+      <hr class="tracklist-divider" />
+
+      <div class="tracklist-body"></div>
+    `;
+  }
+}
