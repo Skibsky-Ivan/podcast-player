def11887e5c0e315e2ee9f5557009c690ed19a01
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-08-06 19:20:21 +0300

    feat(main) add routes for details and playlist pages (Thu, Aug 06, 2026 07:20:21 PM)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { router } from './core/router.ts';
 import { MainLayout } from './layout/main-layout.ts';
+import { DetailsPage } from './pages/details.ts';
+import { PlaylistPage } from './pages/playlist.ts';
 import { LandingPage } from './pages/landing.ts';
 
 router.addLayout('/', MainLayout);
 
 router.addRoute('/', LandingPage);
+
+router.addRoute('/details/:id', DetailsPage);
+
+router.addRoute('/playlist', PlaylistPage);
