3db44ba64dd994cbb43afe3b82d1137489a3971f
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-08-08 01:37:33 +0300

    refactor(types): extend Episode interface and add PlaylistItem type (Sat, Aug 08, 2026 01:37:33 AM)

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,6 +9,15 @@ export interface Episode {
   id: string;
   title: string;
   duration: string;
+  durationSec: number | null;
+  pubDate: string;
   audioUrl: string;
   coverUrl: string;
+}
+
+export interface PlaylistItem {
+  episode: Episode;
+  podcastId: string;
+  podcastTitle: string;
+  podcastCoverUrl: string;
 }
