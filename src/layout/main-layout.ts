0a829f22f043014b45d60a858061bd075c8a7665
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-08-10 01:27:34 +0300

    feat(main-layout): add Player component (Mon, Aug 10, 2026 01:27:34 AM)

--- src/layout/main-layout.ts
+++ src/layout/main-layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '../core/component';
 import { Header } from './header';
+import { Player } from './player';
 
 export class MainLayout extends Component {
   private header: Header | null = null;
+  private player: Player | null = null;
 
   render(): string {
     return `
@@ -16,15 +18,22 @@ export class MainLayout extends Component {
 
   afterRender(): void {
     const headerSlot = this.element.querySelector('#header-slot');
-
     if (headerSlot && !this.header) {
       this.header = new Header();
       this.header.mount(headerSlot as HTMLElement);
+    }
+
+    const playerSlot = this.element.querySelector('#player-slot');
+    if (playerSlot && !this.player) {
+      this.player = new Player();
+      this.player.mount(playerSlot as HTMLElement);
     }
   }
 
   onUnmount(): void {
     this.header?.unmount();
     this.header = null;
+    this.player?.unmount();
+    this.player = null;
   }
 }
